dcbff9ead38f4dc776301d5361b652ab73147fd1
aitornalla <aitorz1@hotmail.com>
2020-03-25 10:17:41 +0100

    f-Springboard v2
    Added some changes to applied spring force to player
    Added arrow to point force direction

--- Assets/Scripts/AnimatedSpringboardController/AnimatedSpringboard.cs
+++ Assets/Scripts/AnimatedSpringboardController/AnimatedSpringboard.cs
@@ -10,18 +10,26 @@ namespace Assets.Scripts.AnimatedSpringboardController
 		private LayerMask _playerLayer;                 // Player layer to check conditions
 		[SerializeField]
 		private float _springForce = 1000.0f;           // Spring force
+		[SerializeField]
+		private float _angleForce = 90.0f;              // Angle used to apply the force (in degrees)
+        [SerializeField]
+        private bool _totalVelocityCancelation = false; // Flag for total velocity cancellation before applying spring force
 
 		private Animator _animator;
+        private Transform _arrowChild;
+        private const float ArrowRotation_0 = 45.0f;
 
         private void Awake()
         {
 			_animator = gameObject.GetComponent<Animator>();
+
+            _arrowChild = gameObject.transform.GetChild(0);
         }
 
         // Use this for initialization
         private void Start()
 		{
-
+            _arrowChild.localRotation = Quaternion.Euler(0.0f, 0.0f, _angleForce + ArrowRotation_0);
 		}
 
 		// Update is called once per frame
@@ -32,7 +40,7 @@ namespace Assets.Scripts.AnimatedSpringboardController
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-			// If player jumps on the springboard
+			// If player jumps on the springboard ...
 			if (_playerLayer == (_playerLayer | (1 << collision.gameObject.layer)))
 			{
 				// Get RigidBody2D component
@@ -40,17 +48,93 @@ namespace Assets.Scripts.AnimatedSpringboardController
 
                 if (l_rigidbody2D != null)
                 {
-					// Set velocity y-component to 0.0f
-					l_rigidbody2D.velocity = new Vector2(l_rigidbody2D.velocity.x, 0.0f);
-					// Add force
-					l_rigidbody2D.AddForce(new Vector2(0.0f, _springForce));
+                    if (_totalVelocityCancelation)
+                    {
+                        // Set x/y velocity components to zero before applying spring force
+                        l_rigidbody2D.velocity = Vector2.zero;
+                    }
+                    else
+                    {
+                        // Cancel velocity component in the same direction as spring force
+                        l_rigidbody2D.velocity = CancelVelocityOnForceDirection(l_rigidbody2D.velocity);
+                    }
 
+					// Add force
+					float l_anlgeRad = Mathf.Deg2Rad * _angleForce;
+					Vector2 l_force = new Vector2(_springForce * Mathf.Cos(l_anlgeRad), _springForce * Mathf.Sin(l_anlgeRad));
+					l_rigidbody2D.AddForce(l_force);
+
+                    // Trigger animated springboard animation
                     if (_animator != null)
                     {
 						_animator.SetTrigger("Activate");
                     }
 				}
 			}
+        }
+
+        /// <summary>
+        ///     Cancels velocity component in the same direction as the spring force
+        /// </summary>
+        /// <param name="rbVelocity">Velocity vector</param>
+        /// <returns>Velocity vector transformed with canceled component</returns>
+        private Vector2 CancelVelocityOnForceDirection(Vector2 rbVelocity)
+        {
+            Vector2 l_canceledVel = Vector2.zero;
+            float l_alpha = 0.0f; //(_angleForce - 90.0f) * Mathf.Deg2Rad;
+            float l_beta_raw = Mathf.Atan2(Mathf.Abs(rbVelocity.y), Mathf.Abs(rbVelocity.x));
+            float l_beta = 0.0f;
+
+            // Set beta angle correctly from raw beta angle
+            if (Mathf.Sign(rbVelocity.x) == 1.0f && Mathf.Sign(rbVelocity.y) == 1.0f)
+            {
+                l_beta = l_beta_raw;
+            }
+
+            if (Mathf.Sign(rbVelocity.x) == -1.0f && Mathf.Sign(rbVelocity.y) == 1.0f)
+            {
+                l_beta = Mathf.PI - l_beta_raw;
+            }
+
+            if (Mathf.Sign(rbVelocity.x) == -1.0f && Mathf.Sign(rbVelocity.y) == -1.0f)
+            {
+                l_beta = Mathf.PI + l_beta_raw;
+            }
+
+            if (Mathf.Sign(rbVelocity.x) == 1.0f && Mathf.Sign(rbVelocity.y) == -1.0f)
+            {
+                l_beta = 2.0f * Mathf.PI - l_beta_raw;
+            }
+
+            ///
+
+            if (Mathf.Sign(Mathf.Cos(_angleForce)) == -1.0f && Mathf.Sign(Mathf.Cos(l_beta)) == 1.0f)
+            {
+                l_alpha = (_angleForce - 90.0f) * Mathf.Deg2Rad;
+            }
+
+            if (Mathf.Sign(Mathf.Cos(_angleForce)) == -1.0f && Mathf.Sign(Mathf.Cos(l_beta)) == -1.0f)
+            {
+                l_alpha = (_angleForce + 90.0f) * Mathf.Deg2Rad;
+            }
+
+            if (Mathf.Sign(Mathf.Cos(_angleForce)) == 1.0f && Mathf.Sign(Mathf.Cos(l_beta)) == 1.0f)
+            {
+                l_alpha = (_angleForce + 270.0f) * Mathf.Deg2Rad;
+            }
+
+            if (Mathf.Sign(Mathf.Cos(_angleForce)) == 1.0f && Mathf.Sign(Mathf.Cos(l_beta)) == -1.0f)
+            {
+                l_alpha = (_angleForce + 90.0f) * Mathf.Deg2Rad;
+            }
+
+            ///
+
+            Vector2 l_vF = new Vector2(rbVelocity.magnitude * Mathf.Cos(l_alpha + l_beta), 0.0f);
+
+            l_canceledVel = new Vector2(l_vF.magnitude * Mathf.Cos(l_alpha), l_vF.magnitude * Mathf.Sin(l_alpha));
+
+            return l_canceledVel;
         }
     }
 }
