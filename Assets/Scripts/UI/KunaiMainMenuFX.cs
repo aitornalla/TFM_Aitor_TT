c2b7e2bdff34ad0270491926eb1c3f61b81f7cd9
aitornalla <aitorz1@hotmail.com>
2020-04-28 18:44:13 +0200

    f-MainMenu
    Added kunai through screen FX

---
+++ Assets/Scripts/UI/KunaiMainMenuFX.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+	public class KunaiMainMenuFX : MonoBehaviour
+	{
+		[SerializeField]
+		private float _kunaiSpeedLower = 10.0f;                                 // Lower kunai speed limit
+		[SerializeField]
+		private float _kunaiSpeedUpper = 20.0f;                                 // Upper kunai speed limit
+		[SerializeField]
+		private float _kunaiRotationSpeed = -1750.0f;                           // Kunai rotation speed
+
+		private float _kunaiSpeed = 0.0f;
+		private float _initialRotation = 0.0f;
+		private bool _kunaiRotates = false;
+
+		// Use this for initialization
+		private void Start()
+		{
+			_kunaiSpeed = _kunaiSpeedLower + Random.value * (_kunaiSpeedUpper - _kunaiSpeedLower);
+
+			_kunaiRotates = (int)(Random.value * 1000) % 2 == 0;
+
+			if (_kunaiRotates)
+			{
+				Destroy(this.gameObject, 10.0f);
+
+				_initialRotation = transform.eulerAngles.z;
+			}
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+            if (_kunaiRotates)
+            {
+				transform.Translate(
+                    _kunaiSpeed * Time.deltaTime * Mathf.Cos(_initialRotation * Mathf.Deg2Rad),
+					_kunaiSpeed * Time.deltaTime * Mathf.Sin(_initialRotation * Mathf.Deg2Rad),
+                    0.0f, Space.World);
+
+				transform.Rotate(0.0f, 0.0f, _kunaiRotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+				transform.Translate(_kunaiSpeed * Time.deltaTime, 0.0f, 0.0f, Space.Self);
+			}
+		}
+
+        private void OnBecameInvisible()
+        {
+            if (!_kunaiRotates)
+			    Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuFXSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+	public class MainMenuFXSpawner : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject _kunaiMainMenuPrefab;                                // Reference to kunai main menu prefab
+		[SerializeField]
+		private float _kunaiSpawnRadii = 15.0f;                                 // Kunai spawn radii
+		[SerializeField]
+		private float _kunaiFXSpawnTimeLower = 3.0f;                            // Time between kunai spawns (lower limit)
+		[SerializeField]
+		private float _kunaiFXSpawnTimeUpper = 6.0f;                            // Time between kunai spawns (upper limit)
+
+
+		// Use this for initialization
+		private void Start()
+		{
+			StartCoroutine(KunaiMainMenuFXCoroutine());
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+
+		}
+
+		private IEnumerator KunaiMainMenuFXCoroutine()
+		{
+			float l_angle = Random.value * 360.0f;
+
+			GameObject l_prefab = Instantiate(_kunaiMainMenuPrefab);
+
+			l_prefab.transform.position = new Vector3(_kunaiSpawnRadii * Mathf.Cos(l_angle * Mathf.Deg2Rad), _kunaiSpawnRadii * Mathf.Sin(l_angle * Mathf.Deg2Rad), 0.0f);
+
+			l_prefab.transform.Rotate(0.0f, 0.0f, l_angle + 180.0f);
+
+			yield return new WaitForSeconds(_kunaiFXSpawnTimeLower + Random.value * (_kunaiFXSpawnTimeUpper - _kunaiFXSpawnTimeLower));
+
+			StartCoroutine(KunaiMainMenuFXCoroutine());
+		}
+	}
+}
