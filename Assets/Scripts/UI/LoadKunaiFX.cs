442078cedb4a94853fa62781d1c477c9fd7ed4a1
aitornalla <aitorz1@hotmail.com>
2020-05-03 18:46:10 +0200

    f-Load-Kunai-FX
    Added rotating kunai with trail particle system FX in loading screen

---
+++ Assets/Scripts/UI/LoadKunaiFX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+	public class LoadKunaiFX : MonoBehaviour
+	{
+		[SerializeField]
+		private float _rotationSpeed;
+
+        // Use this for initialization
+        private void Start()
+		{
+			StartCoroutine(LoadKunaiFXCoroutine(Time.realtimeSinceStartup));
+		}
+
+		//private void FixedUpdate()
+		//{
+		//	transform.Rotate(0.0f, 0.0f, Time.fixedDeltaTime * _rotationSpeed, Space.Self);
+		//}
+
+        private IEnumerator LoadKunaiFXCoroutine(float lastTime)
+        {
+			float l_time = Time.realtimeSinceStartup;
+			float l_deltaTime = l_time - lastTime;
+
+			transform.Rotate(0.0f, 0.0f, l_deltaTime * _rotationSpeed, Space.Self);
+
+			yield return null;
+
+			StartCoroutine(LoadKunaiFXCoroutine(l_time));
+		}
+	}
+}
