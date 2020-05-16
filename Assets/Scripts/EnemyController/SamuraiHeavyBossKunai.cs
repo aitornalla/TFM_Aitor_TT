1bc651ab1ff82bfdea62f1df564029973292e1ea
aitornalla <aitorz1@hotmail.com>
2020-05-16 16:02:35 +0200

    f-BossKunai
    Added trail effect to kunais in boss level

--- Assets/Scripts/EnemyController/SamuraiHeavyBossKunai.cs
+++ Assets/Scripts/EnemyController/SamuraiHeavyBossKunai.cs
@@ -14,9 +14,15 @@ namespace Assets.Scripts.EnemyController
 		[SerializeField]
 		private int _damage = 5;                                                // Kunai damage
 		[SerializeField]
+		private ParticleSystem _trailFarticleSystem;                            // Reference to trail ParticleSystem component
+		[SerializeField]
+		private Transform _finalTrailPoint;                                     // Referende to the point where the trail disappear
+        [SerializeField]
 		private AudioSource _audioSource;                                       // Reference to AudioSource component
         [SerializeField]
 		private AudioClip[] _swishAudioClips;                                   // Swish sounds
+
+		private bool _waitingToDestroy = false;                                 // Flag for destroying the gameObject
 
 		// Use this for initialization
 		private void Start()
@@ -32,7 +38,8 @@ namespace Assets.Scripts.EnemyController
 
         private void FixedUpdate()
         {
-			transform.Translate(_speed * Time.fixedDeltaTime, 0.0f, 0.0f, Space.Self);
+            if (!_waitingToDestroy)
+			    transform.Translate(_speed * Time.fixedDeltaTime, 0.0f, 0.0f, Space.Self);
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
@@ -46,9 +53,23 @@ namespace Assets.Scripts.EnemyController
 				l_characterHealth.TakeDamage(_damage);
 			}
 			// Destroy kunai
-			Destroy(this.gameObject);
+			//Destroy(this.gameObject);
+
+            // Set destroy flag to true
+            _waitingToDestroy = true;
+            // Disable SpriteRenderer component
+            GetComponent<SpriteRenderer>().enabled = false;
+            // Disable CapsuleCollider2D component
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            // Move trail gameObject to its final point
+            _trailFarticleSystem.transform.position = _finalTrailPoint.position;
+            // Start destroy coroutine
+            StartCoroutine(DestroyCoroutine());
 		}
 
+        /// <summary>
+        ///     Plays swish sound
+        /// </summary>
 		private void PlaySwishSound()
 		{
 			float l_value = Random.value * 1000.0f;
@@ -67,6 +88,20 @@ namespace Assets.Scripts.EnemyController
 			{
 				_audioSource.PlayOneShot(_swishAudioClips[2]);
 			}
+		}
+
+        /// <summary>
+        ///     Coroutine that waits for the trail particle system to finish before destroying the gameObject
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator DestroyCoroutine()
+        {
+            while (_trailFarticleSystem.isPlaying)
+            {
+				yield return null;
+            }
+			// Destroy kunai
+			Destroy(this.gameObject);
 		}
     }
 }
